0e3666d1220aa9b183c60ac9d45b9cad6a36e09d
Maxim Lippeveld <maxim.lippeveld@ugent.be>
2020-05-14 00:11:01 +0200

    update appconfig

--- singlecell/apps.py
+++ singlecell/apps.py
@@ -1,11 +1,5 @@
 from django.apps import AppConfig
-import javabridge
-import bioformats
 
 
 class SinglecellConfig(AppConfig):
     name = 'singlecell'
-
-    def ready(self):
-        # javabridge.start_vm(class_path=bioformats.JARS)
-        pass
